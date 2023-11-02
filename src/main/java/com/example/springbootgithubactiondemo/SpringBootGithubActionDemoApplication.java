906841b766e85637e122e7a4732f6ec8695c1aa6
Mohib <gazi.mohib@brainstation-23.com>
2023-11-02 09:36:01 +0000

    initialized demo hello world project

---
+++ src/main/java/com/example/springbootgithubactiondemo/SpringBootGithubActionDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootgithubactiondemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGithubActionDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootGithubActionDemoApplication.class, args);
+    }
+
+}
